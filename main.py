f52f4348554f1e6d29110853613278e3bcf17623
jose carlos flores moran <josecarlosfloresmoran@hotmail.com>
2020-01-12 21:39:52 -0700

    ya jala pero toma los valores de la grafica anterior

--- main.py
+++ main.py
@@ -3,13 +3,28 @@ import plotly.graph_objects as go
 
 csv = pd.read_csv('csv/wind_aristeomercado_10m_complete')
 
-csv = csv[csv.val > 0]
-csv = csv[csv.val < 100]
+i = 0
+inicio = 1
+fin = 0
+while i < len(csv):
+    if csv.iloc[i]['val'] < 0 and csv.iloc[i - 1]['val'] > 0:
+        fin = i
+        df2 = csv.iloc[:inicio-1, :fin-1]
+        print("rango de la grafica " + str(inicio) + " - " + str(fin))
+        fig = go.Figure(go.Scatter(x=df2['time'], y=df2['val'], name='Velocidad'))
 
-fig = go.Figure(go.Scatter(x=csv['time'], y=csv['val'], name='Velocidad'))
+        fig.update_layout(title='Valores de velocidad del veinto en el tiempo',
+                          plot_bgcolor='rgb(230, 230,230)',
+                          showlegend=True)
 
-fig.update_layout(title='Valores de velocidad del veinto en el tiempo',
-                  plot_bgcolor='rgb(230, 230,230)',
-                  showlegend=True)
+        fig.show()
+        inicio = fin
+    i = i + 1
 
-fig.show()
+# fig = go.Figure(go.Scatter(x=csv['time'], y=csv['val'], name='Velocidad'))
+#
+# fig.update_layout(title='Valores de velocidad del veinto en el tiempo',
+#                   plot_bgcolor='rgb(230, 230,230)',
+#                   showlegend=True)
+#
+# fig.show()
