2c768fbbee972a8bd4bb9dcf7bdb9263d6acbe04
jose carlos flores moran <josecarlosfloresmoran@hotmail.com>
2020-01-12 23:49:09 -0700

    subete plis

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import pandas as pd
-import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 
-csv = pd.read_csv('csv/wind_aristeomercado_10m_complete')
+colnames = ['time', 'val']
+csv = pd.read_csv('csv/dataset_wind_aristeomercado_10m_complete.csv', names=colnames, header=None)
 
 i = 0
 inicio = 1
 fin = 0
+plt.xticks(rotation=90)
 while i < len(csv):
     if csv.iloc[i]['val'] < 0 and csv.iloc[i - 1]['val'] > 0:
         fin = i
-        df2 = csv.iloc[:inicio-1, :fin-1]
+        df2 = csv.iloc[inicio-1 : fin-1, 0 : 2]
         print("rango de la grafica " + str(inicio) + " - " + str(fin))
-        fig = go.Figure(go.Scatter(x=df2['time'], y=df2['val'], name='Velocidad'))
+        df2.val = pd.to_numeric(df2.val)
+        df2.plot(kind='line', x='time', y='val')
 
-        fig.update_layout(title='Valores de velocidad del veinto en el tiempo',
-                          plot_bgcolor='rgb(230, 230,230)',
-                          showlegend=True)
-
-        fig.show()
+        plt.savefig(str(inicio) + " - " + str(fin) + '.png')
+        # fig = go.Figure(go.Scatter(x=df2['time'], y=df2['val'], name='Velocidad'))
+        #
+        # fig.update_layout(title='Valores de velocidad del veinto en el tiempo',
+        #                   plot_bgcolor='rgb(230, 230,230)',
+        #                   showlegend=True)
+        #
+        # fig.show()
         inicio = fin
     i = i + 1
 
